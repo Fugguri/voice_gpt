b1c06ae33711159f5f1e8181684bc009ba06bd64
Fugguri <fygguri@icloud.com>
2023-09-08 15:22:31 +0300

    update

--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -4,8 +4,6 @@ def text_to_speech(text,voice_id):
 
     headers = {"Accept": "application/json", "Content-Type": "application/json",
                 "Authorization": "f9f65ea1-4534-41f3-9d8c-e97d97e77823"}
-
-    print(text)
     body = {'voice_id':voice_id,
                 'text': text,
                 'format': 'wav'}
@@ -13,7 +11,6 @@ def text_to_speech(text,voice_id):
     url = "https://api.voice.steos.io/v1/get/tts"
     response = httpx.post(url, headers=headers, json=body)
     data = response.json()
-    print(data)
     path = wget.download(data["audio_url"],"voice")
     
     return path
