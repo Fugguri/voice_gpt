44afcd06df95ce48f6a6be9637f4efc3493bf4af
Fugguri <fygguri@gmail.com>
2024-01-14 16:44:07 +0300

    update

--- utils/_openai.py
+++ utils/_openai.py
@@ -1,9 +1,28 @@
 import asyncio
+from openai import OpenAI
+import openai
+import base64
+import requests
+from openai import OpenAI
+import httpx
+from openai import RateLimitError
+from openai import OpenAI
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+proxy = config["proxy"]
+openai = OpenAI(
+    api_key=config['openAi'],
+
+    http_client=httpx.Client(
+        proxies=proxy,
+        transport=httpx.HTTPTransport(local_address="0.0.0.0"),
+    ),
+)
 
 users_message = {}
 
 
-async def create_responce(message, role_settings, openai, text):
+async def create_responce(message, role_settings, _, text):
     try:
         try:
             users_message[message.from_user.id]
@@ -15,11 +34,11 @@ async def create_responce(message, role_settings, openai, text):
                 "role": "user", "content": role_settings, }]
         users_message[message.from_user.id].append(
             {"role": "user", "content": text})
-        responce = openai.ChatCompletion.create(
+        responce = openai.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=users_message[message.from_user.id]
         )
-        answer = responce['choices'][0]['message']['content']
+        answer = responce.choices[0].message.content
         users_message[message.from_user.id].append(
             {"role": "assistant", "content": answer})
     except openai.error.RateLimitError as ex:
@@ -32,4 +51,4 @@ async def create_responce(message, role_settings, openai, text):
         users_message[message.from_user.id] = []
         await message.reply("Не понимаю, сформулируйте подругому")
 
-    return responce['choices'][0]['message']['content']
+    return responce.choices[0].message.content
